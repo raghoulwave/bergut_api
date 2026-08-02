518041719605f6d5ccac92eab1042fabc834b97d
raghoulwave <raghoulwave@gmail.com>
2026-08-02 20:13:55 +0200

    initial commit

---
+++ src/main/java/ua/raghoulwave/bergut_api/BergutApiApplication.java
@@ -0,0 +1,13 @@
+package ua.raghoulwave.bergut_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BergutApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BergutApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/raghoulwave/bergut_api/BergutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.raghoulwave.bergut_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BergutApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
